309d59c4ebaebdd8efadedd35752ea820efe713e
chenjt <2434130846@qq.com>
2023-05-09 15:09:25 +0800

    webpack entry and output

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
-// mode 
-// development 开发模型，代码未经过压缩
-// production  生产模型，代码经过压缩
-// node 默认为生产模式
+
 
 module.exports = {
+  // mode 
+  // development 开发模型，代码未经过压缩
+  // production  生产模型，代码经过压缩
+  // node 默认为生产模式
   mode: 'development',
+  
   // 单个文件输入
   // entry: './src/index.js',
   // 多个文件
@@ -14,12 +16,12 @@ module.exports = {
     main: './src/index.js',
     search: './src/search.js',
   },
+
   // 单个文件输出
   // output: {
   //   path: path.resolve(__dirname, 'dist'),
   //   filename: 'bundle.js',
   // },
-
   // 多个文件输出
   output: {
     // __dirname 获取当前绝对路径
